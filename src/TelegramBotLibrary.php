6e8ecd36ba15c047e22edd9b045c9598d6eea253
Protocol Corporation <protocollive@outlook.com>
2022-03-13 20:16:54 -0300

    SendPhoto method

--- src/TelegramBotLibrary.php
+++ src/TelegramBotLibrary.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.03.12.01
+//2022.03.13.00
 
 require(__DIR__ . '/requires.php');
 
@@ -468,7 +468,117 @@ class TelegramBotLibrary extends TblBasics{
   }
 
   /**
-   * Use this method to edit text and game messages. On success, if the edited message is not an inline message, the edited Message is returned, otherwise True is returned.
+   * Use this method to send photos.
+   * @param int $Chat Unique identifier for the target chat
+   * @param string $Photo Photo to send. Pass a file_id as String to send a photo that exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get a photo from the Internet, or upload a new photo using multipart/form-data. The photo must be at most 10 MB in size. The photo's width and height must not exceed 10000 in total. Width and height ratio must be at most 20.
+   * There are three ways to send files (photos, stickers, audio, media, etc.):
+   * 1) If the file is already stored somewhere on the Telegram servers, you don't need to reupload it: each file object has a file_id field, simply pass this file_id as a parameter instead of uploading. There are no limits for files sent this way.
+   * 2) Provide Telegram with an HTTP URL for the file to be sent. Telegram will download and send the file. 5 MB max size for photos and 20 MB max for other types of content.
+   * 3) Post the file using multipart/form-data in the usual way that files are uploaded via the browser. 10 MB max size for photos, 50 MB for other files.
+   * 
+   * Sending by file_id
+   * - It is not possible to change the file type when resending by file_id. I.e. a video can't be sent as a photo, a photo can't be sent as a document, etc.
+   * - It is not possible to resend thumbnails.
+   * - Resending a photo by file_id will send all of its sizes.
+   * - file_id is unique for each individual bot and can't be transferred from one bot to another.
+   * - file_id uniquely identifies a file, but a file can have different valid file_ids even for the same bot.
+   * 
+   * Sending by URL
+   * - When sending by URL the target file must have the correct MIME type (e.g., audio/mpeg for sendAudio, etc.).
+   * - Other configurations may work but we can't guarantee that they will.
+   * @param string $Caption Photo caption (may also be used when resending photos by file_id), 0-1024 characters after entities parsing
+   * @param TgParseMode $ParseMode Mode for parsing entities in the photo caption. See formatting options for more details.
+   * @param array $Entities A list of special entities that appear in the caption, which can be specified instead of parse_mode
+   * @param bool $DisableNotification Sends the message silently. Users will receive a notification with no sound.
+   * @param bool $Protect Protects the contents of the sent message from forwarding and saving
+   * @param int $RepliedMsg If the message is a reply, ID of the original message
+   * @param bool $SendWithoutRepliedMsg Pass True, if the message should be sent even if the specified replied-to message is not found
+   * @param TblMarkup $Markup Additional interface options. A object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
+   * @return TgMessage|null On success, the sent Message is returned.
+   * @link https://core.telegram.org/bots/api#sendphoto
+   */
+  public function SendPhoto(
+    int $Chat,
+    string $Photo,
+    string $Caption = null,
+    TgParseMode $ParseMode = TgParseMode::Html,
+    array $Entities = null,
+    bool $DisableNotification = false,
+    bool $Protect = false,
+    int $RepliedMsg = null,
+    bool $SendWithoutRepliedMsg = false,
+    TblMarkup $Markup = null
+  ):TgMessage|null{
+    if($Caption !== null
+    and strlen($Caption) > TgLimits::Caption):
+      $this->Error = TblError::LimitPhotoCaption;
+      return null;
+    endif;
+    $param['chat_id'] = $Chat;
+    if($Caption !== null):
+      $param['caption'] = $Caption;
+      $param['parse_mode'] = $ParseMode->value;
+    endif;
+    if($Entities !== null):
+      $param['caption_entities'] = TblEntities::ToJson($Entities);
+    endif;
+    if($DisableNotification):
+      $param['disable_notification'] = 'true';
+    endif;
+    if($Protect):
+      $param['protect_content'] = 'true';
+    endif;
+    if($RepliedMsg !== null):
+      $param['reply_to_message_id'] = $RepliedMsg;
+    endif;
+    if($SendWithoutRepliedMsg):
+      $param['allow_sending_without_reply'] = 'true';
+    endif;
+    if($Markup !== null):
+      $param['reply_markup'] = $Markup->Get();
+    endif;
+    if(is_file($Photo)):
+      $url = $this->BotData->UrlFiles . '/sendPhoto?' . http_build_query($param);
+      $curl = curl_init($url);
+      curl_setopt($curl, CURLOPT_CAINFO, __DIR__ . '/cacert.pem');
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, [
+        'photo' => new CurlFile($Photo)
+      ]);
+      curl_setopt($curl, CURLOPT_INFILESIZE, filesize($Photo));
+      $temp = curl_exec($curl);
+      if($temp === false):
+        $this->DebugLog(
+          TblLog::Error,
+          'cURL error #' . curl_errno($curl) . ' ' . curl_error($curl)
+        );
+        $this->Error = TblError::Curl;
+        return null;
+      endif;
+      $temp = json_decode($temp, true);
+      if(($this->BotData->Debug & TblDebug::Send) === TblDebug::Send):
+        $this->DebugLog(TblLog::Send, $url . http_build_query($param));
+        $this->DebugLog(TblLog::Send, json_encode($temp, JSON_PRETTY_PRINT));
+      endif;
+      if($temp['ok'] === false):
+        $this->Error = TblError::Custom;
+        $this->ErrorStr = $temp['description'];
+        return null;
+      else:
+        return new TgMessage($temp);
+      endif;
+    else:
+      $param['photo'] = $Photo;
+      $temp = $this->ServerMethod('sendPhoto?' . http_build_query($param));
+      if($temp === null):
+        return null;
+      else:
+        return new TgMessage($temp);
+      endif;
+    endif;
+  }
+
    * @param int $Chat Required if inline_message_id is not specified. Unique identifier for the target chat
    * @param int $Id Required if inline_message_id is not specified. Identifier of the message to edit
    * @param string $Text New text of the message, 1-4096 characters after entities parsing
