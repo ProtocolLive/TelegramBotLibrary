7397c9d50b2e459e27f4fc05f393d14a098e21dc
Protocol Corporation <protocollive@outlook.com>
2022-03-18 15:36:58 -0300

    Class for command

--- src/TblObjects/cmd.php
+++ src/TblObjects/cmd.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.03.06.00
+//2022.03.18.00
 
 class TblCmd extends TgMessage{
   public readonly string $Command;
@@ -33,5 +33,40 @@ class TblCmd extends TgMessage{
       $this->Target = substr($this->Command, $pos + 1);
       $this->Command = substr($this->Command, 0, $pos);
     endif;
+  }
+}
+
+class TblCommand{
+  public readonly string $Command;
+  public readonly string $Description;
+
+  /**
+   * @param string $Command
+   * @param string $Description
+   */
+  public function __construct(
+    string $Command,
+    string $Description
+  ){
+    if(strlen($Command) > TgLimits::Command):
+      $this->Error = TblError::LimitCommand;
+      return;
+    elseif(strlen($Description) > TgLimits::CmdDescription):
+      $this->Error = TblError::LimitCmdDescription;
+      return;
+    endif;
+    $this->Command = $Command;
+    $this->Description = $Description;
+  }
+
+  static public function ToJson(array $Data):string{
+    $return = [];
+    foreach($Data as $cmd):
+      $return[] = [
+        'command' => $cmd->Command,
+        'description' => $cmd->Description
+      ];
+    endforeach;
+    return json_encode($return);
   }
 }
