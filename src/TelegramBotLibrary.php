c48d8aa57d7150d48a5a258d1278a843b97d2f1e
Protocol Corporation <protocollive@outlook.com>
2022-03-30 09:44:27 -0300

    Method CallbackAnswer

--- src/TelegramBotLibrary.php
+++ src/TelegramBotLibrary.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.03.18.06
+//2022.03.30.00
 
 require(__DIR__ . '/requires.php');
 
@@ -728,5 +728,39 @@ class TelegramBotLibrary extends TblBasics{
     $file = file_get_contents($file);
     file_put_contents($Dir . '/' . basename($info->Path), $file);
     return true;
+  }
+
+  /**
+   * Use this method to send answers to callback queries sent from inline keyboards. The answer will be displayed to the user as a notification at the top of the chat screen or as an alert.
+   * Alternatively, the user can be redirected to the specified Game URL. For this option to work, you must first create a game for your bot via @Botfather and accept the terms. Otherwise, you may use links like t.me/your_bot?start=XXXX that open your bot with a parameter.
+   * @param string $Id Unique identifier for the query to be answered
+   * @param string $Text Text of the notification. If not specified, nothing will be shown to the user, 0-200 characters
+   * @param bool $Alert If True, an alert will be shown by the client instead of a notification at the top of the chat screen. Defaults to false.
+   * @param string $Url URL that will be opened by the user's client. If you have created a Game and accepted the conditions via @Botfather, specify the URL that opens your game — note that this will only work if the query comes from a callback_game button. Otherwise, you may use links like t.me/your_bot?start=XXXX that open your bot with a parameter.
+   * @param int $Cache The maximum amount of time in seconds that the result of the callback query may be cached client-side. Telegram apps will support caching starting in version 3.14. Defaults to 0.
+   * @return bool|null On success, True is returned.
+   * @link https://core.telegram.org/bots/api#answercallbackquery
+   */
+  public function CallbackAnswer(
+    string $Id,
+    string $Text = null,
+    bool $Alert = false,
+    string $Url = null,
+    int $Cache = null
+  ):bool|null{
+    $param['callback_query_id'] = $Id;
+    if($Text !== null):
+      $param['text'] = $Text;
+    endif;
+    if($Alert):
+      $param['show_alert'] = 'true';
+    endif;
+    if($Url !== null):
+      $param['url'] = $Url;
+    endif;
+    if($Cache !== null):
+      $param['cache_time'] = $Cache;
+    endif;
+    return $this->ServerMethod('answerCallbackQuery?' . http_build_query($param));
   }
 }
