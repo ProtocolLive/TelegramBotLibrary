6ce55abd3bc2e68fd54335068914e65292100041
Protocol Corporation <protocollive@outlook.com>
2022-04-14 14:40:37 -0300

    New errors

--- src/TgObjects/errors.php
+++ src/TgObjects/errors.php
@@ -1,15 +1,21 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.03.30.00
+//2022.04.11.00
 
 enum TgError{
+  case Blocked;
   case CallbackQueryOld;
+  case FileId;
+  case Url;
 }
 
 class TgErrors{
   private const Errors = [
-    'Bad Request: query is too old and response timeout expired or query ID is invalid' => TgError::CallbackQueryOld
+    'Forbidden: bot was blocked by the user' => TgError::Blocked,
+    'Bad Request: query is too old and response timeout expired or query ID is invalid' => TgError::CallbackQueryOld,
+    'Bad Request: failed to get HTTP URL content' => TgError::Url,
+    'Bad Request: wrong file identifier\/HTTP URL specified' => TgError::FileId,
   ];
 
   static public function Search(string $Description):TgError|false{
