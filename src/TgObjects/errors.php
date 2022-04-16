d10e4235d90abcba34f0d21cb452b0d7c96eaa9a
Protocol Corporation <protocollive@outlook.com>
2022-04-16 13:25:31 -0300

    New error

--- src/TgObjects/errors.php
+++ src/TgObjects/errors.php
@@ -1,13 +1,14 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.04.11.00
+//2022.04.16.00
 
 enum TgError{
   case Blocked;
   case CallbackQueryOld;
   case FileId;
   case Url;
+  case PermAdminManage;
 }
 
 class TgErrors{
@@ -16,6 +17,7 @@ class TgErrors{
     'Bad Request: query is too old and response timeout expired or query ID is invalid' => TgError::CallbackQueryOld,
     'Bad Request: failed to get HTTP URL content' => TgError::Url,
     'Bad Request: wrong file identifier\/HTTP URL specified' => TgError::FileId,
+    'Bad Request: can\'t parse ChatAdministratorRights: Field \"can_manage_chat\" must be of type Boolean' => TgError::PermAdminManage
   ];
 
   static public function Search(string $Description):TgError|false{
