031a791bf94652f3bb9cea11edf7eecac7727990
Protocol Corporation <protocollive@outlook.com>
2022-04-21 15:32:55 -0300

    Updated docs and added certificate

--- src/TblObjects/webhook.php
+++ src/TblObjects/webhook.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.04.20.00
+//2022.04.21.00
 
 class TblWebhook extends TblBasics{
   public function __construct(TblData $BotData){
@@ -9,16 +9,21 @@ class TblWebhook extends TblBasics{
   }
 
   /**
-   * @param string $Url HTTPS url to send updates to.
+   * Use this method to specify a url and receive incoming updates via an outgoing webhook. Whenever there is an update for the bot, we will send an HTTPS POST request to the specified url, containing a JSON-serialized Update. In case of an unsuccessful request, we will give up after a reasonable amount of attempts. If you'd like to make sure that the Webhook request comes from Telegram, we recommend using a secret path in the URL, e.g. https://www.example.com/<token>. Since nobody else knows your bot's token, you can be pretty sure it's us.
+   * @param string $Url HTTPS url to send updates to. Use an empty string to remove webhook integration
    * @param string $ServerIp The fixed IP address which will be used to send webhook requests instead of the IP address resolved through DNS
    * @param int $MaxConnections Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery, 1-100. Defaults to 40. Use lower values to limit the load on your bot's server, and higher values to increase your bot's throughput.
+   * @param array $Updates A list of the update types you want your bot to receive. For example, specify [“message”, “edited_channel_post”, “callback_query”] to only receive updates of these types. See Update for a complete list of available update types. Specify an empty list to receive all update types except chat_member (default). If not specified, the previous setting will be used. Please note that this parameter doesn't affect updates created before the call to the setWebhook, so unwanted updates may be received for a short period of time.
+   * @param string $Certificate Upload your public key certificate so that the root certificate in use can be checked. See our self-signed guide for details.
+   * @return bool|null Returns True on success.
    * @link https://core.telegram.org/bots/api#setwebhook
    */
   public function Set(
     string $Url,
     string $ServerIp = null,
     int $MaxConnections = null,
-    array $Updates = null
+    array $Updates = null,
+    string $Certificate = null
   ):bool|null{
     $param['url'] = $Url;
     if($ServerIp !== null):
@@ -34,6 +39,9 @@ class TblWebhook extends TblBasics{
         $Updates
       );
       $param['allowed_updates'] = json_encode($Updates);
+    endif;
+    if($Certificate !== null):
+      $param['certificate'] = new CurlFile($Certificate);
     endif;
     return $this->ServerMethod('setWebhook', $param);
   }
