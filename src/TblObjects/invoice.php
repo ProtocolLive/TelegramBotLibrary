ca646607fab627f2be3ee0c903a5f0a59a03eb5e
Protocol Corporation <protocollive@outlook.com>
2022-04-20 21:49:21 -0300

    Shipping options and ignore limits for discounts and shipping

--- src/TblObjects/invoice.php
+++ src/TblObjects/invoice.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.04.19.00
+//2022.04.20.01
 
 class TblInvoiceProduct{
   /**
@@ -9,17 +9,37 @@ class TblInvoiceProduct{
    */
   public function __construct(
     public readonly string $Name,
-    public readonly int $Price
+    public readonly int $Price,
+    bool $IgnoreLimit = false
   ){
-    $CurrenciesJson = file_get_contents('https://core.telegram.org/bots/payments/currencies.json');
-    if($CurrenciesJson !== false):
-      $CurrenciesJson = json_decode($CurrenciesJson, true);
-      if($Price < $CurrenciesJson[DefaultCurrency->value]['min_amount']):
-        throw new Exception('Price too low', TblException::InvoicePriceLow);
-      endif;
-      if($Price > $CurrenciesJson[DefaultCurrency->value]['max_amount']):
-        throw new Exception('Price too high', TblException::InvoicePriceHigh);
+    if($IgnoreLimit === false):
+      $CurrenciesJson = file_get_contents('https://core.telegram.org/bots/payments/currencies.json');
+      if($CurrenciesJson !== false):
+        $CurrenciesJson = json_decode($CurrenciesJson, true);
+        if($Price > 0):
+          if($Price < $CurrenciesJson[DefaultCurrency->value]['min_amount']):
+            throw new Exception('Price too low', TblException::InvoicePriceLow);
+          endif;
+          if($Price > $CurrenciesJson[DefaultCurrency->value]['max_amount']):
+            throw new Exception('Price too high', TblException::InvoicePriceHigh);
+          endif;
+        endif;
       endif;
     endif;
   }
+}
+
+/**
+ * This object represents one shipping option.
+ * @param string $Id Shipping option identifier
+ * @param string $Title Option title
+ * @param array $Prices List of price portions, in TblInvoiceProduct format
+ * @link https://core.telegram.org/bots/api#shippingoption
+ */
+class TblInvoiceShippingOption{
+  public function __construct(
+    public readonly string $Id,
+    public readonly string $Title,
+    public readonly array $Prices
+  ){}
 }
