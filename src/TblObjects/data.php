142622097e58d8a765ea6bf8c6c5c73d438b85e3
Protocol Corporation <protocollive@outlook.com>
2022-04-22 16:17:53 -0300

    Added test server option

--- src/TblObjects/data.php
+++ src/TblObjects/data.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.03.05.00
+//2022.04.22.00
 
 class TblData{
   public readonly string $UrlApi;
@@ -12,10 +12,15 @@ class TblData{
   public function __construct(
     string $Token,
     string $DirLogs,
-    int $Debug = TblDebug::None
+    int $Debug = TblDebug::None,
+    bool $TestServer = false
   ){
     $this->UrlApi = 'https://api.telegram.org/bot' . $Token;
     $this->UrlFiles = 'https://api.telegram.org/file/bot' . $Token;
+    if($TestServer):
+      $this->UrlApi .= '/test';
+      $this->UrlFiles .= '/test';
+    endif;
     $this->DirLogs = $DirLogs;
     $this->Debug = $Debug;
   }
