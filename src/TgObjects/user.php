9347621ac77b78bd75fce42023d5223dc16c5af8
Protocol Corporation <protocollive@outlook.com>
2022-06-21 09:23:32 -0300

    Added premium and attachment parameters

--- src/TgObjects/user.php
+++ src/TgObjects/user.php
@@ -1,7 +1,8 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.03.06.00
+//2022.06.21.00
+//API 6.1
 
 /**
  * @link https://core.telegram.org/bots/api#user
@@ -10,6 +11,8 @@ class TgUser{
   public readonly int $Id;
   public readonly string $Name;
   public readonly bool $Bot;
+  public readonly bool $Premium;
+  public readonly bool $Attached;
   public readonly string|null $NameLast;
   public readonly string|null $Nick;
   public readonly string|null $Language;
@@ -21,6 +24,8 @@ class TgUser{
     $this->Id = $Data['id'];
     $this->Name = $Data['first_name'];
     $this->Bot = $Data['is_bot'] ?? false;
+    $this->Premium = $Data['is_premium'] ?? false;
+    $this->Attached = $Data['added_to_attachment_menu'] ?? false;
     $this->NameLast = $Data['last_name'] ?? null;
     $this->Nick = $Data['username'] ?? null;
     $this->Language = $Data['language_code'] ?? null;
