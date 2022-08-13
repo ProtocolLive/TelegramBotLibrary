bb350437ec84f4c7773ef3eaa5c79f5d73f91fae
Protocol Corporation <protocollive@outlook.com>
2022-08-13 14:01:27 -0300

    Docs improved

--- src/TgObjects/poll.php
+++ src/TgObjects/poll.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.04.24.00
+//2022.08.13.00
 
 enum TgPollType:string{
   case Regular = 'regular';
@@ -14,16 +14,49 @@ enum TgPollType:string{
  */
 class TgPoll{
   public readonly TgMessage|null $Message;
+  /**
+   * Unique poll identifier
+   */
   public readonly string $PollId;
+  /**
+   * Poll type, currently can be “regular” or “quiz”
+   */
   public readonly TgPollType $Type;
+  /**
+   * Poll question, 1-300 characters
+   */
   public readonly string $Question;
+  /**
+   * If the poll allows multiple answers
+   */
   public readonly bool $Multiple;
+  /**
+   * List of poll options
+   */
   public array $Options = [];
+  /**
+   * Total number of users that voted in the poll
+   */
   public readonly int $Votes;
+  /**
+   * If the poll is closed
+   */
   public readonly bool $Closed;
+  /**
+   * If the poll is anonymous
+   */
   public readonly bool $Anonymous;
+  /**
+   * 0-based identifier of the correct answer option. Available only for polls in the quiz mode, which are closed, or was sent (not forwarded) by the bot or to the private chat with the bot.
+   */
   public readonly int|null $Answer;
+  /**
+   * Text that is shown when a user chooses an incorrect answer or taps on the lamp icon in a quiz-style poll, 0-200 characters
+   */
   public readonly string|null $Explanation;
+  /**
+   * Special entities like usernames, URLs, bot commands, etc. that appear in the explanation
+   */
   public array $ExplanationEntities = [];
 
   /**
@@ -56,15 +89,19 @@ class TgPoll{
 }
 
 /**
+ * This object contains information about one answer option in a poll.
  * @link https://core.telegram.org/bots/api#polloption
  */
 class TgPollOption{
+  /**
+   * Option text, 1-100 characters
+   */
   public readonly string $Text;
+  /**
+   * Number of users that voted for this option
+   */
   public readonly int $Votes;
 
-  /**
-   * @link https://core.telegram.org/bots/api#polloption
-   */
   public function __construct(array $Data){
     $this->Text = $Data['text'];
     $this->Votes = $Data['voter_count'];
