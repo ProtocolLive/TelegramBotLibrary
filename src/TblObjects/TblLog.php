adb0910841b8e4b89d82bb8c7e6b074d1ea0fa6b
Protocol Corporation <protocollive@outlook.com>
2022-08-28 19:08:35 -0300

    File renamed

--- src/TblObjects/TblLog.php
+++ src/TblObjects/TblLog.php
@@ -1,13 +1,15 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.08.18.00
+//2022.08.28.00
 
 namespace ProtocolLive\TelegramBotLibrary\TblObjects;
 
-enum TblLog{
-  case Error;
-  case Webhook;
-  case Send;
-  case Response;
+class TblLog{
+  const All = -1;
+  const None = 0;
+  const Send = 1;
+  const Response = 2;
+  const Webhook = 4;
+  const Curl = 8;
 }
