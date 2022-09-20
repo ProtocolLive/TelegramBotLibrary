229fe235af2e028e3b01d4bd719b4eb0b3ccb403
Protocol Corporation <protocollive@outlook.com>
2022-09-20 19:43:52 -0300

    Added default currency

--- src/TblObjects/TblInvoicePrices.php
+++ src/TblObjects/TblInvoicePrices.php
@@ -1,9 +1,10 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.08.19.00
+//2022.09.20.00
 
 namespace ProtocolLive\TelegramBotLibrary\TblObjects;
+use ProtocolLive\TelegramBotLibrary\TgObjects\TgInvoiceCurrencies;
 
 class TblInvoicePrices{
   private array $Prices = [];
@@ -12,18 +13,19 @@ class TblInvoicePrices{
   public function Add(
     string $Name,
     int $Price,
-    bool $IgnoreLimit = false
+    bool $IgnoreLimit = false,
+    TgInvoiceCurrencies $Currency = TgInvoiceCurrencies::USD
   ):bool{
     if($IgnoreLimit === false):
       $CurrenciesJson = file_get_contents('https://core.telegram.org/bots/payments/currencies.json');
       if($CurrenciesJson !== false):
         $CurrenciesJson = json_decode($CurrenciesJson, true);
         if($Price > 0):
-          if($Price <= $CurrenciesJson[DefaultCurrency->value]['min_amount']):
+          if($Price <= $CurrenciesJson[$Currency->value]['min_amount']):
             $this->Error = TblError::InvoicePriceLow;
             return false;
           endif;
-          if($Price > $CurrenciesJson[DefaultCurrency->value]['max_amount']):
+          if($Price > $CurrenciesJson[$Currency->value]['max_amount']):
             $this->Error = TblError::InvoicePriceHigh;
             return false;
           endif;
