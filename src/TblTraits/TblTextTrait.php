ab69da41688e96f971a5853b23761dbfd2ed372c
Protocol Corporation <protocollive@outlook.com>
2022-11-05 11:27:51 -0300

    Created trait for Text

---
+++ src/TblTraits/TblTextTrait.php
@@ -0,0 +1,180 @@
+<?php
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive/TelegramBotLibrary
+//2022.11.05.00
+
+namespace ProtocolLive\TelegramBotLibrary\TblTraits;
+use ProtocolLive\TelegramBotLibrary\TblObjects\TblEntities;
+use ProtocolLive\TelegramBotLibrary\TblObjects\TblMarkup;
+use ProtocolLive\TelegramBotLibrary\TgObjects\TgMethods;
+use ProtocolLive\TelegramBotLibrary\TgObjects\TgParseMode;
+use ProtocolLive\TelegramBotLibrary\TgObjects\TgText;
+
+trait TblTextTrait{
+  /**
+   * Use this method to edit text and game messages.
+   * @param int $Chat Required if inline_message_id is not specified. Unique identifier for the target chat
+   * @param int $Id Required if inline_message_id is not specified. Identifier of the message to edit
+   * @param string $Text New text of the message, 1-4096 characters after entities parsing
+   * @param string $InlineId Required if chat_id and message_id are not specified. Identifier of the inline message
+   * @param TgParseMode $ParseMode Mode for parsing entities in the message text.
+   * @param TblEntities $Entities A list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param bool $DisablePreview Disables link previews for links in this message
+   * @param TblMarkup $Markup A object for an inline keyboard.
+   * @return TgMessage|bool On success, if the edited message is not an inline message, the edited Message is returned, otherwise True is returned.
+   * @throws TblException
+   * @link https://core.telegram.org/bots/api#editmessagetext
+   */
+  public function TextEdit(
+    int $Chat = null,
+    int $Id = null,
+    string $Text,
+    string $InlineId = null,
+    TgParseMode $ParseMode = TgParseMode::Html,
+    TblEntities $Entities = null,
+    bool $DisablePreview = false,
+    TblMarkup $Markup = null
+  ):TgText|bool{
+    if($Chat !== null):
+      $param['chat_id'] = $Chat;
+    endif;
+    if($Id !== null):
+      $param['message_id'] = $Id;
+    endif;
+    $param['text'] = $Text;
+    if($InlineId !== null):
+      $param['inline_message_id'] = $InlineId;
+    endif;
+    $param['parse_mode'] = $ParseMode->value;
+    if($Entities !== null):
+      $param['entities'] = json_encode($Entities);
+    endif;
+    if($DisablePreview):
+      $param['disable_web_page_preview'] = 'true';
+    endif;
+    if($Markup !== null):
+      $param['reply_markup'] = $Markup->ToJson();
+    endif;
+    $return = $this->ServerMethod(TgMethods::TextEdit, $param);
+    if($return === true):
+      return $return;
+    else:
+      return new TgText($return);
+    endif;
+  }
+
+  /**
+   * Use this method to send text messages for one or multiple chats. The Telegram API have some limits, but don't have a official docs for this.
+   * @param int|array $Chat Unique identifier for the target chats
+   * @param string $Text Text of the message to be sent, 1-4096 characters after entities parsing
+   * @param TgParseMode $ParseMode Mode for parsing entities in the message text.
+   * @param TblEntities $Entities A list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param bool $DisablePreview Disables link previews for links in this message
+   * @param bool $DisableNotification Sends the message silently. Users will receive a notification with no sound.
+   * @param bool $Protect Protects the contents of the sent message from forwarding and saving
+   * @param int $RepliedMsg If the message is a reply, ID of the original message
+   * @param bool $SendWithoutRepliedMsg Pass True, if the message should be sent even if the specified replied-to message is not found
+   * @param array TblMarkup Additional interface options. A object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
+   * @return TgMessage On success, the sent Message is returned.
+   * @throws TblException
+   * @link https://core.telegram.org/bots/api#sendmessage
+   */
+  public function TextSend(
+    int $Chat,
+    string $Text,
+    TgParseMode $ParseMode = null,
+    TblEntities $Entities = null,
+    bool $DisablePreview = false,
+    bool $DisableNotification = false,
+    bool $Protect = false,
+    int $RepliedMsg = null,
+    bool $SendWithoutRepliedMsg = false,
+    TblMarkup $Markup = null
+  ):TgText{
+    $param = $this->TextSendArgs(
+      $Chat,
+      $Text,
+      $ParseMode,
+      $Entities,
+      $DisablePreview,
+      $DisableNotification,
+      $Protect,
+      $RepliedMsg,
+      $SendWithoutRepliedMsg,
+      $Markup
+    );
+    $return = $this->ServerMethod(TgMethods::TextSend, $param);
+    return new TgText($return);
+  }
+
+  /**
+   * Use this method with TextSendMulti
+   * @param int|array $Chat Unique identifier for the target chats
+   * @param string $Text Text of the message to be sent, 1-4096 characters after entities parsing
+   * @param TgParseMode $ParseMode Mode for parsing entities in the message text.
+   * @param TblEntities $Entities A list of special entities that appear in message text, which can be specified instead of parse_mode
+   * @param bool $DisablePreview Disables link previews for links in this message
+   * @param bool $DisableNotification Sends the message silently. Users will receive a notification with no sound.
+   * @param bool $Protect Protects the contents of the sent message from forwarding and saving
+   * @param int $RepliedMsg If the message is a reply, ID of the original message
+   * @param bool $SendWithoutRepliedMsg Pass True, if the message should be sent even if the specified replied-to message is not found
+   * @param array TblMarkup Additional interface options. A object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
+   * @return array Prepared parameters for the TextSendMulti method
+   * @link https://core.telegram.org/bots/api#sendmessage
+   */
+  public function TextSendArgs(
+    int $Chat,
+    string $Text,
+    TgParseMode $ParseMode = null,
+    TblEntities $Entities = null,
+    bool $DisablePreview = false,
+    bool $DisableNotification = false,
+    bool $Protect = false,
+    int $RepliedMsg = null,
+    bool $SendWithoutRepliedMsg = false,
+    TblMarkup $Markup = null,
+  ):array{
+    $param['chat_id'] = $Chat;
+    $param['text'] = $Text;
+    if($ParseMode !== null):
+      $param['parse_mode'] = $ParseMode->value;
+    endif;
+    if($Entities !== null):
+      $param['entities'] = json_encode($Entities);
+    endif;
+    if($DisablePreview):
+      $param['disable_web_page_preview'] = true;
+    endif;
+    if($DisableNotification):
+      $param['disable_notification'] = true;
+    endif;
+    if($Protect):
+      $param['protect_content'] = true;
+    endif;
+    if($RepliedMsg !== null):
+      $param['reply_to_message_id'] = $RepliedMsg;
+    endif;
+    if($SendWithoutRepliedMsg):
+      $param['allow_sending_without_reply'] = true;
+    endif;
+    if($Markup !== null):
+      $param['reply_markup'] = $Markup->ToJson();
+    endif;
+    return $param;
+  }
+
+  /**
+   * Send text to many chats at once. Carefully with server limits.
+   * https://core.telegram.org/bots/faq#my-bot-is-hitting-limits-how-do-i-avoid-this
+   * @return TgText[]|TblException[]
+   */
+  public function TextSendMulti(array $Params):array{
+    $return = $this->ServerMethodMulti(TgMethods::TextSend, $Params);
+    foreach($return as &$answer):
+      if(is_object($answer) === false):
+        $answer = new TgText($answer);
+      endif;
+    endforeach;
+    return $return;
+  }
+}
