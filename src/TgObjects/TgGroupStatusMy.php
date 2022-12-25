0b329225e7de52746700ff7684855bec3995749f
Protocol Corporation <protocollive@outlook.com>
2022-12-25 10:05:24 -0300

    Group can be null

--- src/TgObjects/TgGroupStatusMy.php
+++ src/TgObjects/TgGroupStatusMy.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.12.23.00
+//2022.12.25.00
 
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
 
@@ -15,9 +15,9 @@ class TgGroupStatusMy{
    */
   public readonly TgUser $User;
   /**
-   * Chat the user belongs to. Can be null in case of bot are blocked
+   * Chat the user belongs to
    */
-  public readonly TgChat|null $Group;
+  public readonly TgChat $Group;
   /**
    * Date the change was done in Unix time
    */
@@ -38,11 +38,7 @@ class TgGroupStatusMy{
    */
   public function __construct(array $Data){
     $this->User = new TgUser($Data['from']);
-    if($Data['chat']['type'] === TgChatType::Private->value):
-      $this->Group = null;
-    else:
-      $this->Group = new TgChat($Data['chat']);
-    endif;
+    $this->Group = new TgChat($Data['chat']);
     $this->Date = $Data['date'];
     $this->StatusOld = TgMemberStatus::tryFrom($Data['old_chat_member']['status']);
     $this->StatusNew = TgMemberStatus::tryFrom($Data['new_chat_member']['status']);
