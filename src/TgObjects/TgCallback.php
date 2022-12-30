61667cab77b4775d13231e05a6a318cc37693bdc
Protocol Corporation <protocollive@outlook.com>
2022-12-30 18:36:50 -0300

    Remaking the properties

--- src/TgObjects/TgCallback.php
+++ src/TgObjects/TgCallback.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.12.30.00
+//2022.12.30.01
 
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
 
@@ -9,6 +9,10 @@ namespace ProtocolLive\TelegramBotLibrary\TgObjects;
  * @link https://core.telegram.org/bots/api#callbackquery
  */
 final class TgCallback{
+  /**
+   * Message with the callback button that originated the query. Note that message content and message date will not be available if the message is too old
+   */
+  public readonly TgText $Data;
   /**
    * Unique identifier for this query
    */
@@ -18,27 +22,19 @@ final class TgCallback{
    */
   public readonly TgUser $User;
   /**
-   * Message with the callback button that originated the query. Note that message content and message date will not be available if the message is too old
-   */
-  public readonly TgText $Message;
-  /**
    * Global identifier, uniquely corresponding to the chat to which the message with the callback button was sent. Useful for high scores in games.
    */
   public readonly string $ChatInstance;
   /**
    * Data associated with the callback button. Be aware that the message originated the query can contain no callback buttons with this data.
    */
-  public readonly string $Data;
-
-  public readonly string|null $Parameter;
+  public readonly string $Callback;
 
   public function __construct(array $Data){
     $this->Id = $Data['id'];
     $this->User = new TgUser($Data['from']);
-    $this->Message = new TgText($Data['message']);
+    $this->Data = new TgText($Data['message']);
     $this->ChatInstance = $Data['chat_instance'];
-    $temp = explode(' ', $Data['data']);
-    $this->Data = $temp[0];
-    $this->Parameter = $temp[1] ?? null;
+    $this->Callback = $Data['data'];
   }
 }
