79c3cc20108914a8f11ddd1cc81f003377c5fdd5
Protocol Corporation <protocollive@outlook.com>
2023-01-16 10:52:01 -0300

    Alphabetical sort, don't escape slashes and get markups as array

--- src/TblObjects/TblMarkup.php
+++ src/TblObjects/TblMarkup.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.08.19.00
+//2023.01.16.00
 
 namespace ProtocolLive\TelegramBotLibrary\TblObjects;
 
@@ -9,17 +9,21 @@ abstract class TblMarkup{
   protected array $Markup;
   protected array $Pointer;
 
-  /**
-   * Get the markup object in json format
-   */
-  public function ToJson():string{
-    return json_encode($this->Markup);
-  }
-
   public function ButtonGet(
     int $Line,
     int $Col
   ):array{
     return $this->Pointer[$Line][$Col];
+  }
+
+  /**
+   * Get the markup object in json format
+   */
+  public function ToJson():string{
+    return json_encode($this->Markup, JSON_UNESCAPED_SLASHES);
+  }
+
+  public function ToArray():array{
+    return $this->Markup;
   }
 }
