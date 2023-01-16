b48330d4eb441f52d04cd26131b6e711edd98aeb
Protocol Corporation <protocollive@outlook.com>
2023-01-16 10:53:30 -0300

    Added docs and ChatType can be null

--- src/TgObjects/TgInlineQuery.php
+++ src/TgObjects/TgInlineQuery.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.08.18.00
+//2023.01.16.00
 
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
 
@@ -10,10 +10,25 @@ namespace ProtocolLive\TelegramBotLibrary\TgObjects;
  * @link https://core.telegram.org/bots/api#inlinequery
  */
 class TgInlineQuery{
+  /**
+   * @param string $Id Unique identifier for this query
+   */
   public readonly string $Id;
+  /**
+   * @param TgUser $User Sender
+   */
   public readonly TgUser $User;
-  public readonly TgChatType $ChatType;
+  /**
+   * @param TgChatType|null $ChatType Type of the chat from which the inline query was sent. Can be either “sender” for a private chat with the inline query sender, “private”, “group”, “supergroup”, or “channel”. The chat type should be always known for requests sent from official clients and most third-party clients, unless the request was sent from a secret chat
+   */
+  public readonly TgChatType|null $ChatType;
+  /**
+   * @param string $Text Text of the query (up to 256 characters)
+   */
   public readonly string $Text;
+  /**
+   * @param string $Offset Offset of the results to be returned, can be controlled by the bot
+   */
   public readonly string $Offset;
 
   /**
@@ -22,7 +37,11 @@ class TgInlineQuery{
   public function __construct(array $Data){
     $this->Id = $Data['id'];
     $this->User = new TgUser($Data['from']);
-    $this->ChatType = TgChatType::tryFrom($Data['chat_type']);
+    if(isset($Data['chat_type'])):
+      $this->ChatType = TgChatType::tryFrom($Data['chat_type']);
+    else:
+      $this->ChatType = null;
+    endif;
     $this->Text = $Data['query'];
     $this->Offset = $Data['offset'];
   }
