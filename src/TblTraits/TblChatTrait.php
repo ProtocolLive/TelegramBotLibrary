0bbc75268fd8b17b3f6ea0ebab1318c9cb88b2f1
Protocol Corporation <protocollive@outlook.com>
2023-01-28 07:43:23 -0300

    Added option to interact with chat with their username

--- src/TblTraits/TblChatTrait.php
+++ src/TblTraits/TblChatTrait.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2021.01.21.00
+//2023.01.28.00
 
 namespace ProtocolLive\TelegramBotLibrary\TblTraits;
 use ProtocolLive\TelegramBotLibrary\TgObjects\{
@@ -19,7 +19,7 @@ trait TblChatTrait{
   /**
    * Use this method when you need to tell the user that something is happening on the bot's side. The status is set for 5 seconds or less (when a message arrives from your bot, Telegram clients clear its typing status).
    * Example: The ImageBot needs some time to process a request and upload the image. Instead of sending a text message along the lines of “Retrieving image, please wait…”, the bot may use sendChatAction with action = upload_photo. The user will see a “sending photo” status for the bot.
-   * @param int $Chat Unique identifier for the target chat
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @param TgChatAction $Action Type of action to broadcast.
    * @param int $Topic Unique identifier for the target message thread; supergroups only
    * @return bool Returns True on success.
@@ -27,7 +27,7 @@ trait TblChatTrait{
    * @link https://core.telegram.org/bots/api#sendchataction
    */
   public function ChatActionSend(
-    int $Chat,
+    int|string $Chat,
     TgChatAction $Action,
     int $Topic = null
   ):bool{
@@ -41,12 +41,14 @@ trait TblChatTrait{
 
   /** MUDAR
    * Use this method to get a list of administrators in a chat. On success, returns an Array of ChatMember objects that contains information about all chat administrators except other bots.
-   * @param int $Chat Unique identifier for the target chat
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @return array If the chat is a group or a supergroup and no administrators were appointed, only the creator will be returned.
    * @throws TblException
    * @link https://core.telegram.org/bots/api#getchatadministrators
    */
-  public function ChatAdmGet(int $Chat):array|null{
+  public function ChatAdmGet(
+    int|string $Chat
+  ):array{
     $param['chat_id'] = $Chat;
     $temp = $this->ServerMethod(TgMethods::ChatAdms, $param);
     $return = [];
@@ -58,7 +60,7 @@ trait TblChatTrait{
 
   /**
    * Use this method to promote or demote a user in a supergroup or a channel. The bot must be an administrator in the chat for this to work and must have the appropriate administrator rights. Pass False for all boolean parameters to demote a user.
-   * @param int $Chat Unique identifier for the target
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @param int $User Unique identifier of the target user
    * @param TgPermAdmin $Perms
    * @param bool $Anonymous If the administrator's presence in the chat is hidden
@@ -67,7 +69,7 @@ trait TblChatTrait{
    * @link https://core.telegram.org/bots/api#promotechatmember
    */
   public function ChatAdminPerm(
-    int $Chat,
+    int|string $Chat,
     int $User,
     TgPermAdmin $Perms,
     bool $Anonymous = false
@@ -83,7 +85,7 @@ trait TblChatTrait{
 
   /**
    * Use this method to set a custom title for an administrator in a supergroup promoted by the bot.
-   * @param int $Chat Unique identifier for the target chat
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @param int $User Unique identifier of the target user
    * @param string $Title New custom title for the administrator; 0-16 characters, emoji are not allowed
    * @return bool Returns True on success.
@@ -91,7 +93,7 @@ trait TblChatTrait{
    * @link https://core.telegram.org/bots/api#setchatadministratorcustomtitle
    */
   public function ChatAdminTitleSet(
-    int $Chat,
+    int|string $Chat,
     int $User,
     string $Title
   ):bool{
@@ -103,7 +105,7 @@ trait TblChatTrait{
 
   /**
    * Use this method to ban a user in a group, a supergroup or a channel. In the case of supergroups and channels, the user will not be able to return to the chat on their own using invite links, etc., unless unbanned first. The bot must be an administrator in the chat for this to work and must have the appropriate administrator rights.
-   * @param int $Chat Unique identifier for the target group or username of the target supergroup or channel
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @param int $User Unique identifier of the target user
    * @param int $Date Date when the user will be unbanned, unix time. If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever. Applied for supergroups and channels only.
    * @param bool $DelMsg Pass True to delete all messages from the chat for the user that is being removed. If False, the user will be able to see messages in the group that were sent before the user was removed. Always True for supergroups and channels.
@@ -112,7 +114,7 @@ trait TblChatTrait{
    * @link https://core.telegram.org/bots/api#banchatmember
    */
   public function ChatBan(
-    int $Chat,
+    int|string $Chat,
     int $User,
     int $Date,
     bool $DelMsg = false
@@ -126,7 +128,7 @@ trait TblChatTrait{
 
   /**
    * Use this method to unban a previously banned user in a supergroup or channel. The user will not return to the group or channel automatically, but will be able to join via link, etc. The bot must be an administrator for this to work. By default, this method guarantees that after the call the user is not a member of the chat, but will be able to join it. So if the user is a member of the chat they will also be removed from the chat. If you don't want this, use the parameter only_if_banned.
-   * @param int $Chat Unique identifier for the target group or username of the target supergroup or channel
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @param int $User Unique identifier of the target user
    * @param bool $OnlyIfBanned Do nothing if the user is not banned
    * @return bool Returns True on success.
@@ -134,7 +136,7 @@ trait TblChatTrait{
    * @link https://core.telegram.org/bots/api#unbanchatmember
    */
   public function ChatBanUndo(
-    int $Chat,
+    int|string $Chat,
     int $User,
     bool $OnlyIfBanned = true
   ):bool{
@@ -146,25 +148,27 @@ trait TblChatTrait{
 
   /**
    * Use this method to get the number of members in a chat.
-   * @param int $Chat Unique identifier for the target
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @param int Returns Int on success.
    * @throws TblException
    * @link https://core.telegram.org/bots/api#getchatmembercount
    */
-  public function ChatCount(int $Chat):int{
+  public function ChatCount(
+    int|string $Chat
+  ):int{
     $param['chat_id'] = $Chat;
     return $this->ServerMethod(TgMethods::ChatMemberCount, $param);
   }
 
   /**
    * Use this method to get up to date information about the chat (current name of the user for one-on-one conversations, current username of a user, group or channel, etc.).
-   * @param int $Chat Unique identifier for the target chat
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @return TgChat|TgUser Returns a Chat object on success.
    * @throws TblException
    * @link https://core.telegram.org/bots/api#getchat
    */
   public function ChatGet(
-    int $Chat
+    int|string $Chat
   ):TgChat|TgUser{
     $param['chat_id'] = $Chat;
     $temp = $this->ServerMethod(TgMethods::Chat, $param);
@@ -177,14 +181,14 @@ trait TblChatTrait{
 
   /**
    * Use this method to get information about a member of a chat.
-   * @param int $Chat Unique identifier for the target chat
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @param int $User Unique identifier of the target user
    * @return TgMember Returns a ChatMember object on success.
    * @throws TblException
    * @link https://core.telegram.org/bots/api#getchatmember
    */
   public function ChatMember(
-    int $Chat,
+    int|string $Chat,
     int $User
   ):TgMember{
     $param['chat_id'] = $Chat;
@@ -195,7 +199,7 @@ trait TblChatTrait{
 
   /**
    * Use this method to restrict a user in a supergroup. The bot must be an administrator in the supergroup for this to work and must have the appropriate administrator rights. Pass True for all permissions to lift restrictions from a user.
-   * @param int $Chat Unique identifier for the target
+   * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @param int $User Unique identifier of the target user
    * @param TgPermMember $Perms A object for new user permissions
    * @param int $Period Date when restrictions will be lifted for the user, unix time. If user is restricted for more than 366 days or less than 30 seconds from the current time, they are considered to be restricted forever
@@ -204,7 +208,7 @@ trait TblChatTrait{
    * @link https://core.telegram.org/bots/api#restrictchatmember
    */
   public function ChatMemberPerm(
-    int $Chat,
+    int|string $Chat,
     int $User,
     TgPermMember $Perms,
     int $Period = null
