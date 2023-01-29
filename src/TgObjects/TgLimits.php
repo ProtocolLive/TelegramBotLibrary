030e85f596cfc43fae14a7e5bcce7bddb22db0dd
Protocol Corporation <protocollive@outlook.com>
2023-01-29 13:21:52 -0300

    Added a doc

--- src/TgObjects/TgLimits.php
+++ src/TgObjects/TgLimits.php
@@ -1,21 +1,22 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.08.31.00
+//2023.01.29.00
 
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
 
 class TgLimits{
+  /**
+   * @link https://core.telegram.org/bots/api#answercallbackquery
+   */
   const CallbackAnswer = 200;
   const CallbackData = 64;
   const Caption = 1024;
   /**
-   * Text of the command; 1-32 characters. Can contain only lowercase English letters, digits and underscores.
    * @link https://core.telegram.org/bots/api#botcommand
    */
   const Command = 32;
   /**
-   * Description of the command; 1-256 characters.
    * @link https://core.telegram.org/bots/api#botcommand
    */
   const CmdDescription = 256;
