90dab6333be9c4f46676e8bd8bc14ab00d14266f
Protocol Corporation <protocollive@outlook.com>
2023-02-08 16:56:51 -0300

    Properties promotion

--- src/TblObjects/TblData.php
+++ src/TblObjects/TblData.php
@@ -1,22 +1,19 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.08.28.00
+//2023.02.08.00
 
 namespace ProtocolLive\TelegramBotLibrary\TblObjects;
 
-class TblData{
+final class TblData{
   public readonly string $UrlApi;
   public readonly string $UrlFiles;
-  public readonly int $Log;
-  public readonly string $DirLogs;
-  public readonly string|null $TokenWebhook;
 
   public function __construct(
     string $Token,
-    string $DirLogs,
-    string $TokenWebhook = null,
-    int $Log = TblLog::None,
+    public readonly string $DirLogs,
+    public readonly string|null $TokenWebhook = null,
+    public readonly int $Log = TblLog::None,
     bool $TestServer = false
   ){
     if($TestServer):
@@ -26,8 +23,5 @@ class TblData{
     endif;
     $this->UrlApi = 'https://api.telegram.org/bot' . $Token . $temp;
     $this->UrlFiles = 'https://api.telegram.org/file/bot' . $Token . $temp;
-    $this->DirLogs = $DirLogs;
-    $this->Log = $Log;
-    $this->TokenWebhook = $TokenWebhook;
   }
 }
