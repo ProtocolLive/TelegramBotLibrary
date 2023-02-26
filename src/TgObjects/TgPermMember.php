05f3184e51882675b30ab61771dbaa4258813e70
Protocol Corporation <protocollive@outlook.com>
2023-02-26 09:24:58 -0300

    Added the properties and docs

--- src/TgObjects/TgPermMember.php
+++ src/TgObjects/TgPermMember.php
@@ -1,14 +1,14 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2023.02.03.00
+//2023.02.26.00
 
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
 
 /**
  * @link https://core.telegram.org/bots/api#chatpermissions
  */
-class TgPermMember{
+final class TgPermMember{
   const Array = [
     'Message' => 'can_send_messages',
     'Media' => 'can_send_media_messages',
@@ -31,6 +31,12 @@ class TgPermMember{
    * Describes actions that a non-administrator user is allowed to take in a chat.
    * @param bool $Message If the user is allowed to send text messages, contacts, locations and venues
    * @param bool $Media If the user is allowed to send audios, documents, photos, videos, video notes and voice notes, implies can_send_messages
+   * @param bool $Audio Ff the user is allowed to send audios
+   * @param bool $Documents If the user is allowed to send documents
+   * @param bool $Photos If the user is allowed to send photos
+   * @param bool $Videos If the user is allowed to send videos
+   * @param bool $VideoNote If the user is allowed to send video notes
+   * @param bool $VoiceNote If the user is allowed to send voice notes
    * @param bool $Poll If the user is allowed to send polls, implies can_send_messages
    * @param bool $Media2 If the user is allowed to send animations, games, stickers and use inline bots, implies $Media
    * @param bool $Preview If the user is allowed to add web page previews to their messages, implies $Media
@@ -42,21 +48,21 @@ class TgPermMember{
    */
   public function __construct(
     array $Data = null,
-    bool $Message = false,
-    bool $Media = false,
-    bool $Audio = false,
-    bool $Documents = false,
-    bool $Photos = false,
-    bool $Videos = false,
-    bool $VideoNote = false,
-    bool $VoiceNote = false,
-    bool $Poll = false,
-    bool $Media2 = false,
-    bool $Preview = false,
-    bool $Info = false,
-    bool $Invite = false,
-    bool $Pin = false,
-    bool $Topics = false
+    public bool $Message = false,
+    public bool $Media = false,
+    public bool $Audio = false,
+    public bool $Documents = false,
+    public bool $Photos = false,
+    public bool $Videos = false,
+    public bool $VideoNote = false,
+    public bool $VoiceNote = false,
+    public bool $Poll = false,
+    public bool $Media2 = false,
+    public bool $Preview = false,
+    public bool $Info = false,
+    public bool $Invite = false,
+    public bool $Pin = false,
+    public bool $Topics = false
   ){
     if($Data !== null):
       foreach(self::Array as $perm => $vector):
