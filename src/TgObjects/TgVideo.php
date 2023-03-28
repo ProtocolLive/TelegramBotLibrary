5f4bb675c095612b313d6edea81e17a5e90e54ba
Protocol Corporation <protocollive@outlook.com>
2023-03-28 09:12:20 -0300

    Fix properties and added docs

--- src/TgObjects/TgVideo.php
+++ src/TgObjects/TgVideo.php
@@ -1,25 +1,55 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2023.03.09.00
+//2023.03.28.00
 
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
 
+/**
+ * @link https://core.telegram.org/bots/api#video
+ */
 class TgVideo{
+  /**
+   * @param TgMessageData $Data Message data
+   */
   public readonly TgMessageData $Data;
+  /**
+   * @param string $Id Identifier for this file, which can be used to download or reuse the file
+   */
   public readonly string $Id;
+  /**
+   * @param string $IdUnique Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+   */
   public readonly string $IdUnique;
+  /**
+   * @param TgPhotoSize $Thumb Video thumbnail
+   */
   public readonly TgPhotoSize $Thumb;
+  /**
+   * @param int $Width Video width as defined by sender
+   */
   public readonly int $Width;
+  /**
+   * @param int $Height Video height as defined by sender
+   */
   public readonly int $Height;
+  /**
+   * @param int $Duration Duration of the video in seconds as defined by sender
+   */
   public readonly int $Duration;
+  /**
+   * @param string $Size File size in bytes. It can be bigger than 2^31 and some programming languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, so a signed 64-bit integer or double-precision float type are safe for storing this value.
+   */
   public readonly string $Size;
+  /**
+   * @param string $Mime MIME type of the file as defined by sender
+   */
   public readonly string $Mime;
 
   public function __construct(array $Data){
     $this->Data = new TgMessageData($Data);
-    $this->FileId = $Data['video']['file_id'];
-    $this->FileIdUnique = $Data['video']['file_unique_id'];
+    $this->Id = $Data['video']['file_id'];
+    $this->IdUnique = $Data['video']['file_unique_id'];
     $this->Thumb = new TgPhotoSize($Data['video']['thumbnail']);
     $this->Width = $Data['video']['width'];
     $this->Height = $Data['video']['height'];
