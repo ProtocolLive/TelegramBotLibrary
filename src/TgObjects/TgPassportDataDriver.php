bae6b3057f5ec08cf8f790ad42a4c83475a21391
Protocol Corporation <protocollive@outlook.com>
2023-05-31 09:23:20 -0300

    Added scans

--- src/TgObjects/TgPassportDataDriver.php
+++ src/TgObjects/TgPassportDataDriver.php
@@ -5,16 +5,20 @@
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
 
 /**
- * @version 2023.05.30.00
+ * @version 2023.05.30.01
  */
 class TgPassportDataDriver{
   public string|null $Data;
   public string|null $Hash;
   public string|null $Number = null;
   public string|null $Expiry = null;
+  public TgPassportFile|null $Front = null;
+  public TgPassportFile|null $Back = null;
 
   public function __construct(array $Data){
     $this->Data = base64_decode($Data['data']);
     $this->Hash = base64_decode($Data['hash']);
+    $this->Front = new TgPassportFile($Data['front_side']);
+    $this->Back = new TgPassportFile($Data['reverse_side']);
   }
 }
