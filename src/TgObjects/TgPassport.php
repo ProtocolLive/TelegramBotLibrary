ec50d8905c94368b78655f0c55e68c226ea1ab86
Protocol Corporation <protocollive@outlook.com>
2023-05-31 12:41:39 -0300

    Doc, nonce, driver's license optional

--- src/TgObjects/TgPassport.php
+++ src/TgObjects/TgPassport.php
@@ -9,7 +9,8 @@ use Exception;
 /**
  * @link https://core.telegram.org/bots/api#passportdata
  * @link https://core.telegram.org/bots/api#encryptedcredentials
- * @version 2023.05.30.01
+ * @link https://core.telegram.org/passport
+ * @version 2023.05.30.02
  */
 final class TgPassport
 extends TgObject{
@@ -26,13 +27,34 @@ extends TgObject{
    * Base64-encoded secret, encrypted with the bot's public RSA key, required for data decryption
    */
   public string|null $Secret;
-
+  /**
+   * Credentials for encrypted personal details
+   */
   public TgPassportDataPersonal $Personal;
-  public TgPassportDataDriver $Driver;
+  /**
+   * Credentials for encrypted driver license
+   */
+  public TgPassportDataDriver|null $Driver = null;
+  /**
+   * Credentials for encrypted residential address
+   */
   public TgPassportDataAddress $Address;
-  public array|null $UtilityBill;
+  /**
+   * Credentials for encrypted utility bill
+   */
+  public array|null $UtilityBill = null;
+  /**
+   * User's verified phone number, available only for “phone_number” type
+   */
   public string $Phone;
+  /**
+   * User's verified email address, available only for “email” type
+   */
   public string $Email;
+  /**
+   * Bot-specified nonce
+   */
+  public string $Nonce;
 
   public function __construct(array $Data = null){
     if($Data !== null):
@@ -57,6 +79,7 @@ extends TgObject{
           endforeach;
         endif;
       endforeach;
+
     endif;
   }
 
@@ -66,7 +89,6 @@ extends TgObject{
   public function Decode(
     string $Privkey
   ):void{
-    //https://core.telegram.org/passport#decrypting-data
     //1) Decrypt the credentials secret
     openssl_private_decrypt(
       $this->Secret,
@@ -83,6 +105,7 @@ extends TgObject{
       $this->Secret,
       $this->Hash
     );
+    $this->Nonce = $Data['nonce'];
     //Decode Address
     $temp = self::DecodeData(
       $this->Address->Data,
@@ -96,13 +119,15 @@ extends TgObject{
     $this->Address->Street1 = $temp['street_line1'];
     $this->Address->Street2 = $temp['street_line2'];
     //Decode driver
-    $temp = self::DecodeData(
-      $this->Driver->Data,
-      base64_decode($Data['secure_data']['driver_license']['data']['secret']),
-      base64_decode($Data['secure_data']['driver_license']['data']['data_hash'])
-    );
-    $this->Driver->Number = $temp['document_no'];
-    $this->Driver->Expiry = $temp['expiry_date'];
+    if($this->Driver !== null):
+      $temp = self::DecodeData(
+        $this->Driver->Data,
+        base64_decode($Data['secure_data']['driver_license']['data']['secret']),
+        base64_decode($Data['secure_data']['driver_license']['data']['data_hash'])
+      );
+      $this->Driver->Number = $temp['document_no'];
+      $this->Driver->Expiry = $temp['expiry_date'];
+    endif;
     //Decode personal
     $temp = self::DecodeData(
       $this->Personal->Data,
@@ -122,8 +147,10 @@ extends TgObject{
     $this->Hash = null;
     $this->Personal->Data = null;
     $this->Personal->Hash = null;
-    $this->Driver->Data = null;
-    $this->Driver->Hash = null;
+    if($this->Driver !== null):
+      $this->Driver->Data = null;
+      $this->Driver->Hash = null;
+    endif;
     $this->Address->Data = null;
     $this->Address->Hash = null;
   }
