cbc507701728a2f8ccd32e4f40bd86fddb60b5f3
Protocol Corporation <protocollive@outlook.com>
2023-12-05 13:44:35 -0300

    Fix return in doc

--- src/TblTraits/TblChatTrait.php
+++ src/TblTraits/TblChatTrait.php
@@ -16,7 +16,7 @@ use ProtocolLive\TelegramBotLibrary\TgObjects\{
 };
 
 /**
- * @version 2023.08.11.00
+ * @version 2023.12.05.00
  */
 trait TblChatTrait{
   /**
@@ -45,7 +45,7 @@ trait TblChatTrait{
   /** MUDAR
    * Use this method to get a list of administrators in a chat. On success, returns an Array of ChatMember objects that contains information about all chat administrators except other bots.
    * @param int|string $Chat Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
-   * @return array If the chat is a group or a supergroup and no administrators were appointed, only the creator will be returned.
+   * @return TgMember[] If the chat is a group or a supergroup and no administrators were appointed, only the creator will be returned.
    * @throws TblException
    * @link https://core.telegram.org/bots/api#getchatadministrators
    */
