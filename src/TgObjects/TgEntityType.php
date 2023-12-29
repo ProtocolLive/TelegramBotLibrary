298b2cafc63db5af323370411d8734c4023cd921
Protocol Corporation <protocollive@outlook.com>
2023-12-29 18:07:54 -0300

    Sorting

--- src/TgObjects/TgEntityType.php
+++ src/TgObjects/TgEntityType.php
@@ -1,38 +1,40 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2022.08.18.00
 
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
 
+/**
+ * @version 2023.12.29.00
+ */
 enum TgEntityType:string{
-  case Mention = 'mention';
-  case Hashtag = 'hashtag';
-  case Cashtag = 'cashtag';
-  case Command = 'bot_command';
-  case CustomEmoji = 'custom_emoji';
-  case Url = 'url';
-  case Email = 'email';
-  case Phone = 'phone_number';
   case Bold = 'bold';
-  case Italic = 'italic';
-  case Underline = 'underline';
-  case Strike = 'strikethrough';
-  case Spoiler = 'spoiler';
+  case Cashtag = 'cashtag';
   /**
    * Monowidth string
    */
   case Code = 'code';
-  /**
-   * Monowidth block
-   */
-  case Pre = 'pre';
+  case Command = 'bot_command';
+  case CustomEmoji = 'custom_emoji';
+  case Email = 'email';
+  case Hashtag = 'hashtag';
+  case Italic = 'italic';
   /**
    * For clickable text URLs
    */
   case Link = 'text_link';
+  case Mention = 'mention';
   /**
    * For users without usernames
    */
   case MentionText = 'text_mention';
+  /**
+   * Monowidth block
+   */
+  case Pre = 'pre';
+  case Phone = 'phone_number';
+  case Spoiler = 'spoiler';
+  case Strike = 'strikethrough';
+  case Underline = 'underline';
+  case Url = 'url';
 }
