a1e204707d651ffbec09400b0a84e5ced9def082
Protocol Corporation <protocollive@outlook.com>
2024-01-01 16:27:38 -0300

    Added anon reaction count

--- src/TgObjects/TgReaction.php
+++ src/TgObjects/TgReaction.php
@@ -7,14 +7,15 @@ use ProtocolLive\TelegramBotLibrary\TgEnums\TgReactionType;
 
 /**
  * @link https://core.telegram.org/bots/api#reactiontype
- * @version 2024.01.01.00
+ * @version 2024.01.01.01
  */
 class TgReaction{
   public readonly TgReactionType $Type;
   public readonly string $Emoji;
 
   public function __construct(
-    array $Data
+    array $Data,
+    public readonly int|null $Count = null
   ){
     $this->Type = TgReactionType::from($Data['type']);
     $this->Emoji = $Data[$this->Type->value];
