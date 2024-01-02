00132ef6f6714a0e957bc5cc6914559e39118cdf
Protocol Corporation <protocollive@outlook.com>
2024-01-02 14:20:00 -0300

    Strange constructor...

--- src/TgObjects/TgEntity.php
+++ src/TgObjects/TgEntity.php
@@ -7,7 +7,7 @@ use ProtocolLive\TelegramBotLibrary\TgEnums\TgEntityType;
 
 /**
  * @link https://core.telegram.org/bots/api#messageentity
- * @version 2024.01.01.00
+ * @version 2024.01.02.00
  */
 class TgEntity{
   public readonly TgEntityType $Type;
@@ -36,43 +36,19 @@ class TgEntity{
    */
   public readonly string|null $CustomEmoji;
   
-  /**
-   * @param TgEntityType $Type Type of the entity
-   * @param int $Offset Offset in UTF-16 code units to the start of the entity
-   * @param int $Length Length of the entity in UTF-16 code units
-   * @param string $Url For “text_link” only, url that will be opened after user taps on the text
-   * @param int $User For “text_mention” only, the mentioned user
-   * @param string $Language For “pre” only, the programming language of the entity text
-   * @link https://core.telegram.org/bots/api#messageentity
-   */
   public function __construct(
-    array $Data = null,
-    TgEntityType $Type = null,
-    int $Offset = null,
-    int $Length = null,
-    string $Url = null,
-    int $User = null,
-    string $Language = null
+    array $Data
   ){
-    if($Data === null):
-      $this->Type = $Type;
-      $this->Offset = $Offset;
-      $this->Length = $Length;
-      $this->Url = $Url;
-      $this->User = $User;
-      $this->Language = $Language;
+    $this->Type = TgEntityType::from($Data['type']);
+    $this->Offset = $Data['offset'];
+    $this->Length = $Data['length'];
+    $this->Url = $Data['url'] ?? null;
+    if(isset($Data['user'])):
+      $this->User = new TgUser($Data['user']);
     else:
-      $this->Type = TgEntityType::from($Data['type']);
-      $this->Offset = $Data['offset'];
-      $this->Length = $Data['length'];
-      $this->Url = $Data['url'] ?? null;
-      if(isset($Data['user'])):
-        $this->User = new TgUser($Data['user']);
-      else:
-        $this->User = null;
-      endif;
-      $this->Language = $Data['language'] ?? null;
-      $this->CustomEmoji = $Data['custom_emoji_id'] ?? null;
+      $this->User = null;
     endif;
+    $this->Language = $Data['language'] ?? null;
+    $this->CustomEmoji = $Data['custom_emoji_id'] ?? null;
   }
 }
