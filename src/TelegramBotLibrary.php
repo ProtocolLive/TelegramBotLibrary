48412116d2901ee664ea4a1e96b088f96389b8b3
Protocol Corporation <protocollive@outlook.com>
2024-01-27 00:19:54 -0300

    Fix docs

--- src/TelegramBotLibrary.php
+++ src/TelegramBotLibrary.php
@@ -60,7 +60,7 @@ use ProtocolLive\TelegramBotLibrary\TgObjects\{
 use ProtocolLive\TelegramBotLibrary\TgParams\TgInlineQueryResults;
 
 /**
- * @version 2024.01.26.00
+ * @version 2024.01.26.01
  */
 final class TelegramBotLibrary
 extends TblBasics{
@@ -486,10 +486,10 @@ extends TblBasics{
   }
 
   /**
-   * Use this method to copy messages of any kind. Service messages and invoice messages can't be copied. The method is analogous to the method forwardMessage, but the copied message doesn't have a link to the original message.
-   * @param int $From
-   * @param int $Id Message identifier in the chat specified in $From
-   * @param int $To Unique identifier for the target chat
+   * Use this method to copy messages of any kind. Service messages, giveaway messages, giveaway winners messages, and invoice messages can't be copied. A quiz poll can be copied only if the value of the field correct_option_id is known to the bot. The method is analogous to the method forwardMessage, but the copied message doesn't have a link to the original message.
+   * @param int $From Unique identifier for the chat where the original message was sent (or channel username in the format @channelusername)
+   * @param int $Id Message identifier in the chat specified in from_chat_id
+   * @param int $To Unique identifier for the target chat or username of the target channel (in the format @channelusername)
    * @param int $Thread Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
    * @param string $Caption New caption for media, 0-1024 characters after entities parsing. If not specified, the original caption is kept
    * @param TblEntities $Entities A list of special entities that appear in the new caption, which can be specified instead of parse_mode
