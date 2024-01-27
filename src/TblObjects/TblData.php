679223a9db272941fb25d61b453ced32b227d8fa
Protocol Corporation <protocollive@outlook.com>
2024-01-27 08:35:28 -0300

    Fix webhook token validation

--- src/TblObjects/TblData.php
+++ src/TblObjects/TblData.php
@@ -5,7 +5,7 @@
 namespace ProtocolLive\TelegramBotLibrary\TblObjects;
 
 /**
- * @version 2024.01.26.00
+ * @version 2024.01.27.00
  */
 final readonly class TblData{
   public string $UrlApi;
@@ -32,7 +32,7 @@ final readonly class TblData{
     $this->UrlApi = 'https://api.telegram.org/bot' . $Token . $temp;
     $this->UrlFiles = 'https://api.telegram.org/file/bot' . $Token . $temp;
     $this->LogHandler = $LogHandler;
-    if(preg_match('/^[a-zA-z0-9_-]{1,256}$/', $TokenWebhook)):
+    if(preg_match('/^[a-zA-z0-9_-]{1,256}$/', $TokenWebhook) === false):
       throw new TblException(TblError::TokenWebhook);
     endif;
     $this->TokenWebhook = $TokenWebhook;
