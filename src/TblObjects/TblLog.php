4db19d842facd1aa620ca8ca60c3dc5279bf2796
Protocol Corporation <protocollive@outlook.com>
2024-01-27 00:11:58 -0300

    Final and public

--- src/TblObjects/TblLog.php
+++ src/TblObjects/TblLog.php
@@ -1,16 +1,18 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/TelegramBotLibrary
-//2024.01.13.00
 
 namespace ProtocolLive\TelegramBotLibrary\TblObjects;
 
-class TblLog{
-  const All = -1;
-  const None = 0;
-  const Send = 1;
-  const Response = 2;
-  const Webhook = 4;
-  const WebhookObject = 8;
-  const Curl = 16;
+/**
+ * @version 2024.01.26.00
+ */
+final class TblLog{
+  public const All = -1;
+  public const None = 0;
+  public const Send = 1;
+  public const Response = 2;
+  public const Webhook = 4;
+  public const WebhookObject = 8;
+  public const Curl = 16;
 }
