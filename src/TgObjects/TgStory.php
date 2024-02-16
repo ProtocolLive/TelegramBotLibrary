d9432cb511910bfea756ce9ae07f78aae47e2c8e
Protocol Corporation <protocollive@outlook.com>
2024-02-16 12:02:32 -0300

    Added Chat and Id

--- src/TgObjects/TgStory.php
+++ src/TgObjects/TgStory.php
@@ -3,6 +3,7 @@
 //https://github.com/ProtocolLive/TelegramBotLibrary
 
 namespace ProtocolLive\TelegramBotLibrary\TgObjects;
+use ProtocolLive\TelegramBotLibrary\TgEnums\TgChatType;
 use ProtocolLive\TelegramBotLibrary\TgInterfaces\{
   TgEventInterface,
   TgForwadableInterface
@@ -10,12 +11,20 @@ use ProtocolLive\TelegramBotLibrary\TgInterfaces\{
 
 /**
  * @link https://core.telegram.org/bots/api#story
- * @version 2024.01.04.00
+ * @version 2024.02.16.00
  */
 final readonly class TgStory
 implements TgEventInterface, TgForwadableInterface{
   public TgMessageData $Data;
-  
+  /**
+   * Chat that posted the story
+   */
+  public TgChat|TgUser $Chat;
+  /**
+   * Unique identifier for the story in the chat
+   */
+  public int $Id;
+
   /**
    * This object represents a message about a forwarded story in the chat. Currently holds no information.
    * @link https://core.telegram.org/bots/api#story
@@ -24,5 +33,11 @@ implements TgEventInterface, TgForwadableInterface{
     array $Data
   ){
     $this->Data = new TgMessageData($Data);
+    if($Data['story']['chat']['type'] === TgChatType::Private->value):
+      $this->Chat = new TgUser($Data['story']['chat']);
+    else:
+      $this->Chat = new TgChat($Data['story']['chat']);
+    endif;
+    $this->Id = $Data['story']['id'];
   }
 }
