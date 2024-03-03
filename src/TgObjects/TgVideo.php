00ad6b259e8375fd00580d2610430e9606783e41
Protocol Corporation <protocollive@outlook.com>
2024-03-03 17:27:24 -0300

    Renamed Thumbnail and can be null

--- src/TgObjects/TgVideo.php
+++ src/TgObjects/TgVideo.php
@@ -11,7 +11,7 @@ use ProtocolLive\TelegramBotLibrary\TgInterfaces\{
 
 /**
  * @link https://core.telegram.org/bots/api#video
- * @version 2024.02.08.01
+ * @version 2024.03.03.00
  */
 readonly class TgVideo
 implements TgCaptionableInterface,
@@ -22,35 +22,35 @@ TgForwadableInterface{
    */
   public TgMessageData|null $Data;
   /**
-   * @param string $Id Identifier for this file, which can be used to download or reuse the file
+   * Identifier for this file, which can be used to download or reuse the file
    */
   public string $Id;
   /**
-   * @param string $IdUnique Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+   * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
    */
   public string $IdUnique;
   /**
-   * @param TgPhotoSize $Thumb Video thumbnail
+   * Video thumbnail
    */
-  public TgPhotoSize $Thumb;
+  public TgPhotoSize|null $Thumbnail;
   /**
-   * @param int $Width Video width as defined by sender
+   * Video width as defined by sender
    */
   public int $Width;
   /**
-   * @param int $Height Video height as defined by sender
+   * Video height as defined by sender
    */
   public int $Height;
   /**
-   * @param int $Duration Duration of the video in seconds as defined by sender
+   * Duration of the video in seconds as defined by sender
    */
   public int $Duration;
   /**
-   * @param string $Size File size in bytes. It can be bigger than 2^31 and some programming languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, so a signed 64-bit integer or double-precision float type are safe for storing this value.
+   * File size in bytes. It can be bigger than 2^31 and some programming languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, so a signed 64-bit integer or double-precision float type are safe for storing this value.
    */
   public string $Size;
   /**
-   * @param string $Mime MIME type of the file as defined by sender
+   * MIME type of the file as defined by sender
    */
   public string $Mime;
 
@@ -64,7 +64,12 @@ TgForwadableInterface{
     endif;
     $this->Id = $Data['video']['file_id'] ?? $Data['file_id'];
     $this->IdUnique = $Data['video']['file_unique_id'] ?? $Data['file_unique_id'];
-    $this->Thumb = new TgPhotoSize($Data['video']['thumbnail'] ?? $Data['thumbnail']);
+    if(isset($Data['video']['thumbnail'])
+    or isset($Data['thumbnail'])):
+      $this->Thumbnail = new TgPhotoSize($Data['video']['thumbnail'] ?? $Data['thumbnail']);
+    else:
+      $this->Thumbnail = null;
+    endif;
     $this->Width = $Data['video']['width'] ?? $Data['width'];
     $this->Height = $Data['video']['height'] ?? $Data['height'];
     $this->Duration = $Data['video']['duration'] ?? $Data['duration'];
