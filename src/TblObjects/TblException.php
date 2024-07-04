6cddc4b5fb429495b2859e2d102195be36305f1e
Protocol Corporation <protocollive@outlook.com>
2024-07-04 18:31:31 -0300

    Type fixed

--- src/TblObjects/TblException.php
+++ src/TblObjects/TblException.php
@@ -4,14 +4,15 @@
 
 namespace ProtocolLive\TelegramBotLibrary\TblObjects;
 use Exception;
+use ProtocolLive\TelegramBotLibrary\TgEnums\TgError;
 
 /**
- * version 2024.01.03.00
+ * version 2024.07.04.00
  */
 class TblException
 extends Exception{
   /**
-   * @param TblError|TgException $code The Exception code.
+   * @param TblError|TgError $code The Exception code.
    * @param string $message [optional] The Exception message to throw.
    * @param Throwable|null $previous [optional] The previous throwable used for the exception chaining.
    * @return void
