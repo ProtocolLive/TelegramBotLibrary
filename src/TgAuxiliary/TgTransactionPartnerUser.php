63f625dfb4379d969b69f0fdc0cca8211b0ba614
Protocol Corporation <protocollive@outlook.com>
2024-08-14 09:57:56 -0300

    Added paid media

--- src/TgAuxiliary/TgTransactionPartnerUser.php
+++ src/TgAuxiliary/TgTransactionPartnerUser.php
@@ -3,12 +3,13 @@
 //https://github.com/ProtocolLive/TelegramBotLibrary
 
 namespace ProtocolLive\TelegramBotLibrary\TgAuxiliary;
+use ProtocolLive\TelegramBotLibrary\TgObjects\TgPaidMedia;
 use ProtocolLive\TelegramBotLibrary\TgObjects\TgUser;
 
 /**
  * Describes a transaction with a user.
  * @link https://core.telegram.org/bots/api#transactionpartneruser
- * @version 2024.07.01.00
+ * @version 2024.08.14.00
  */
 final readonly class TgTransactionPartnerUser
 extends TgTransactionPartner{
@@ -20,11 +21,24 @@ extends TgTransactionPartner{
    * Bot-specified invoice payload
    */
   public string|null $Payload;
+  /**
+   * Information about the paid media bought by the user
+  * @var TgPaidMedia[]
+   */
+  public array|null $PaidMedia;
 
   public function __construct(
     array $Data
   ){
     $this->User = new TgUser($Data['user']);
     $this->Payload = $Data['invoice_payload'] ?? null;
+    if(isset($Data['paid_media'])):
+      foreach($Data['paid_media'] as $media):
+        $temp[] = new TgPaidMedia($media);
+      endforeach;
+      $this->PaidMedia = $temp;
+    else:
+      $this->PaidMedia = null;
+    endif;
   }
 }
