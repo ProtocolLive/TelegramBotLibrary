a53f047ed477f5ad285583346aa86f2a0465cc2d
Protocol Corporation <protocollive@outlook.com>
2024-08-14 13:18:03 -0300

    Added paid reaction

--- src/TgEnums/TgReactionType.php
+++ src/TgEnums/TgReactionType.php
@@ -5,10 +5,12 @@
 namespace ProtocolLive\TelegramBotLibrary\TgEnums;
 
 /**
+ * This object describes the type of a reaction.
  * @link https://core.telegram.org/bots/api#reactiontype
- * @version 2024.01.01.00
+ * @version 2024.08.14.00
  */
 enum TgReactionType:string{
   case Normal = 'emoji';
   case Custom = 'custom_emoji';
+  case Paid = 'paid';
 }
