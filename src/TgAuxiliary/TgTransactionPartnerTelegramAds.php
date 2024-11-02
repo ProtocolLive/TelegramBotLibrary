75c8142398cf3dd6b5104d48cc0559b6985e6bbf
Protocol Corporation <protocollive@outlook.com>
2024-11-01 23:04:18 -0300

    Fix class name

--- src/TgAuxiliary/TgTransactionPartnerTelegramAds.php
+++ src/TgAuxiliary/TgTransactionPartnerTelegramAds.php
@@ -6,8 +6,8 @@ namespace ProtocolLive\TelegramBotLibrary\TgAuxiliary;
 
 /**
  * Describes a withdrawal transaction to the Telegram Ads platform.
- * @link https://core.telegram.org/bots/api#transactionpartnerads
- * @version 2024.07.01.00
+ * @link https://core.telegram.org/bots/api#transactionpartnertelegramads
+ * @version 2024.11.01.00
  */
-final readonly class TgTransactionPartnerAds
+final readonly class TgTransactionPartnerTelegramAds
 extends TgTransactionPartner{}
