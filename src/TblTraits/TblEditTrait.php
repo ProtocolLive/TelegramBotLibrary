c56eba52853e931bf80c4cb998e4b4dd2052d4ad
Protocol Corporation <protocollive@outlook.com>
2024-11-01 22:58:09 -0300

    Update MediaEdit doc

--- src/TblTraits/TblEditTrait.php
+++ src/TblTraits/TblEditTrait.php
@@ -18,7 +18,7 @@ use ProtocolLive\TelegramBotLibrary\TgInterfaces\TgEditedInterface;
 use ProtocolLive\TelegramBotLibrary\TgObjects\TgLimits;
 
 /**
- * @version 2024.07.10.00
+ * @version 2024.11.01.00
  */
 trait TblEditTrait{
   /**
@@ -117,10 +117,10 @@ trait TblEditTrait{
   }
 
   /**
-   * Use this method to edit animation, audio, document, photo, or video messages. If a message is part of a message album, then it can be edited only to an audio for audio albums, only to a document for document albums and to a photo or a video otherwise. When an inline message is edited, a new file can't be uploaded; use a previously uploaded file via its file_id or specify a URL.
-   * @param int $Chat Required if inline_message_id is not specified. Unique identifier for the target chat
+   * Use this method to edit animation, audio, document, photo, or video messages, or to add media to text messages. If a message is part of a message album, then it can be edited only to an audio for audio albums, only to a document for document albums and to a photo or a video otherwise. When an inline message is edited, a new file can't be uploaded; use a previously uploaded file via its file_id or specify a URL. Note that business messages that were not sent by the bot and do not contain an inline keyboard can only be edited within 48 hours from the time they were sent.
+   * @param int $Chat Required if inline_message_id is not specified. Unique identifier for the target chat or username of the target channel (in the format @channelusername)
    * @param int $Id Required if inline_message_id is not specified. Identifier of the message to edit
-   * @param string $InlineIdRequired if chat_id and message_id are not specified. Identifier of the inline message
+   * @param string $InlineIdRequired Required if chat_id and message_id are not specified. Identifier of the inline message
    * @param string $BusinessId Unique identifier of the business connection on behalf of which the message to be edited was sent
    * @param TblMedia $Media A JSON-serialized object for a new media content of the message
    * @param TblMarkup $Markup A JSON-serialized object for a new inline keyboard.
