285c71a9a84d424b07b2d1196c37cc29b95ab869
Protocol Corporation <protocollive@outlook.com>
2024-11-19 15:07:52 -0300

    Note added

--- src/TgInterfaces/TgInlineQueryInterface.php
+++ src/TgInterfaces/TgInlineQueryInterface.php
@@ -5,7 +5,7 @@
 namespace ProtocolLive\TelegramBotLibrary\TgInterfaces;
 
 /**
- * This object represents one result of an inline query. Telegram clients currently support results of the following 20 types: InlineQueryResultCachedAudio, InlineQueryResultCachedDocument, InlineQueryResultCachedGif, InlineQueryResultCachedMpeg4Gif, InlineQueryResultCachedPhoto, InlineQueryResultCachedSticker, InlineQueryResultCachedVideo, InlineQueryResultCachedVoice, InlineQueryResultArticle, InlineQueryResultAudio, InlineQueryResultContact, InlineQueryResultGame, InlineQueryResultDocument, InlineQueryResultGif, InlineQueryResultLocation, InlineQueryResultMpeg4Gif, InlineQueryResultPhoto, InlineQueryResultVenue, InlineQueryResultVideo and InlineQueryResultVoice
+ * This object represents one result of an inline query. Telegram clients currently support results of the following 20 types: InlineQueryResultCachedAudio, InlineQueryResultCachedDocument, InlineQueryResultCachedGif, InlineQueryResultCachedMpeg4Gif, InlineQueryResultCachedPhoto, InlineQueryResultCachedSticker, InlineQueryResultCachedVideo, InlineQueryResultCachedVoice, InlineQueryResultArticle, InlineQueryResultAudio, InlineQueryResultContact, InlineQueryResultGame, InlineQueryResultDocument, InlineQueryResultGif, InlineQueryResultLocation, InlineQueryResultMpeg4Gif, InlineQueryResultPhoto, InlineQueryResultVenue, InlineQueryResultVideo and InlineQueryResultVoice. Note: All URLs passed in inline query results will be available to end users and therefore must be assumed to be public.
  * @link https://core.telegram.org/bots/api#inlinequeryresult
  * @version 2024.01.02.00
  */
