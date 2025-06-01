0501bb606b6e50fa70e1d9f012f8f4351d502a29
Protocol Corporation <protocollive@outlook.com>
2025-06-01 13:18:04 -0300

    Doc mistake and performance improve

--- src/TgObjects/TgProfilePhoto.php
+++ src/TgObjects/TgProfilePhoto.php
@@ -7,27 +7,21 @@ use ProtocolLive\TelegramBotLibrary\TgAuxiliary\TgPhotoSize;
 
 /**
  * @link https://core.telegram.org/bots/api#userprofilephotos
- * @version 2024.04.09.00
+ * @version 2025.06.01.00
  */
 final readonly class TgProfilePhoto{
   public int $Count;
-  /**
-   * @var TgPhotoSize[] $Photos
-   */
   public array $Photos;
 
   public function __construct(
     array $Data
   ){
     $this->Count = $Data['total_count'];
-    $temp = [];
-    foreach($Data['photos'] as $photo1):
-      $Photo = [];
-      foreach($photo1 as $photo2):
-        $Photo[] = new TgPhotoSize($photo2);
+    foreach($Data['photos'] as &$photo1):
+      foreach($photo1 as &$photo2):
+        $photo2 = new TgPhotoSize($photo2);
       endforeach;
-      $temp[] = $Photo;
     endforeach;
-    $this->Photos = $temp;
+    $this->Photos = $Data['photos'];
   }
 }
