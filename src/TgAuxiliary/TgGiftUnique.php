1e0e2554c7f17b5a0c7f4534f213a3ecd4ba67be
Protocol Corporation <protocollive@outlook.com>
2026-02-09 15:35:44 -0300

    New properties

--- src/TgAuxiliary/TgGiftUnique.php
+++ src/TgAuxiliary/TgGiftUnique.php
@@ -9,9 +9,13 @@ use ProtocolLive\TelegramBotLibrary\TgObjects\TgChat;
 /**
  * This object describes a unique gift that was upgraded from a regular gift.
  * @link https://core.telegram.org/bots/api#uniquegift
- * @version 2025.08.15.00
+ * @version 2026.02.09.00
  */
 final readonly class TgGiftUnique{
+  /**
+   * Identifier of the regular gift from which the gift was upgraded
+   */
+  public string $Id;
   /**
    * Human-readable name of the regular gift from which this unique gift was upgraded
    */
@@ -40,13 +44,19 @@ final readonly class TgGiftUnique{
    * Information about the chat that published the gift
    */
   public TgChat|null $Chat;
+  /**
+   * If the gift is assigned from the TON blockchain and can't be resold or transferred in Telegram
+   */
+  public bool $FromBlockchain;
 
   public function __construct(
     array $Data
   ){
+    $this->Id = $Data['gift_id'];
     $this->Name = $Data['base_name'];
     $this->NameUnique = $Data['name'];
     $this->Number = $Data['number'];
+    $this->FromBlockchain = $Data['is_from_blockchain'] ?? false;
     $this->Model = new TgGiftUniqueModel($Data['model']);
     $this->Symbol = new TgGiftUniqueSymbol($Data['symbol']);
     $this->Backdrop = new TgGiftUniqueBackdrop($Data['backdrop']);
