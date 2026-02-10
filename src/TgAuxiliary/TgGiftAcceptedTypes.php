fc88c3361c19ec2096244a154488668ac0b329ef
Protocol Corporation <protocollive@outlook.com>
2026-02-09 22:39:07 -0300

    New property, added docs and fix constructor

--- src/TgAuxiliary/TgGiftAcceptedTypes.php
+++ src/TgAuxiliary/TgGiftAcceptedTypes.php
@@ -7,20 +7,37 @@ namespace ProtocolLive\TelegramBotLibrary\TgAuxiliary;
 /**
  * This object describes the types of gifts that can be gifted to a user or a chat.
  * @link https://core.telegram.org/bots/api#acceptedgifttypes
- * @version 2025.05.11.00
+ * @version 2026.02.09.00
  */
 final readonly class TgGiftAcceptedTypes{
+  /**
+   * If unlimited regular gifts are accepted
+   */
   public bool $Unlimited;
+  /**
+   * If limited regular gifts are accepted
+   */
   public bool $Limited;
+  /**
+   * If unique gifts or gifts that can be upgraded to unique for free are accepted
+   */
   public bool $Unique;
+  /**
+   * If a Telegram Premium subscription is accepted
+   */
   public bool $Premium;
+  /**
+   * If transfers of unique gifts from channels are accepted
+   */
+  public bool $Channel;
 
   public function __construct(
     array $Data
   ){
-    $this->Unlimited = $Data['unlimited_gifts'];
-    $this->Limited = $Data['limited_gifts'];
-    $this->Unique = $Data['unique_gifts'];
-    $this->Premium = $Data['premium_subscription'];
+    $this->Unlimited = $Data['unlimited_gifts'] ?? false;
+    $this->Limited = $Data['limited_gifts'] ?? false;
+    $this->Unique = $Data['unique_gifts'] ?? false;
+    $this->Premium = $Data['premium_subscription'] ?? false;
+    $this->Premium = $Data['gifts_from_channels'] ?? false;
   }
 }
